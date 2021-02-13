b639b68f4de6ee0815923f8a5bd57a9bddaa42ef
oluwaseun <dadsclement@gmail.com>
2021-02-13 15:15:27 +0100

    fix-one

--- client/pages/create-admin.js
+++ client/pages/create-admin.js
@@ -9,7 +9,8 @@ import {
   Button,
   Center,
   Image,
-  Text
+  Text,
+  Flex
 } from "@chakra-ui/react";
 
 const Createadmin = () => {
@@ -18,25 +19,27 @@ const Createadmin = () => {
       <Grid
         templateColumns={"1fr 1fr"}
         sx={{ minHeight: "100vh" }}
-        bg="#EEEAEA"
+        bg="white"
       >
-        <Box>
-            <Text fontSize="4xl">Create Admin Account</Text>
+        <Flex bg="white" pt={12} px={6} w={592} align="center" justify="center">
+           <Box>
+           <Text fontSize="4xl">Create Admin Account</Text>
             <Text fontSize="md">fill out the following information to create an admin account</Text>
-          <FormControl id="email" mr={10} ml={15}>
-            <FormLabel>Company's Name</FormLabel>
+          <FormControl id="email">
+            <FormLabel mt={5}>Company's Name</FormLabel>
             <Input type="text" placeholder="wuralab" size="sm" />
-            <FormLabel>Company's Email</FormLabel>
+            <FormLabel mt={5}>Company's Email</FormLabel>
             <Input type="email" placeholder="user@email.com" size="sm" />
-            <FormLabel>Password</FormLabel>
+            <FormLabel mt={5}>Password</FormLabel>
             <Input type="password" placeholder="password" size="sm" />
-            <FormLabel>Confirm Password</FormLabel>
+            <FormLabel mt={5}>Confirm Password</FormLabel>
             <Input type="password" placeholder="confirm password" size="sm" />
             <Button mt={4} colorScheme="teal" type="submit" size="md">
               Submit
             </Button>
           </FormControl>
-        </Box>
+           </Box>
+        </Flex>
 
         <Box bg="#7742F6">
 
