784f5e7859445235d4a207755820a437a27a0181
Fawas kareem <phawazzzy@gmail.com>
2021-02-26 20:17:09 +0100

    update

---
+++ src/web/pages/create-admin.tsx
@@ -0,0 +1,21 @@
+import Box from '@material-ui/core/Box';
+import Grid, { GridSpacing } from '@material-ui/core/Grid';
+
+const CreateAdmin = () => {
+    return (
+        <Box>
+            <Grid
+  container
+  direction="row"
+  justify="center"
+  alignItems="center">
+            
+            </Grid>
+
+        </Box>
+
+       
+    )
+}
+
+export default CreateAdmin
