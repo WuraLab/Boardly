0a2f987f477aad648149cc6ed66787cdb95b5001
Fawas kareem <phawazzzy@gmail.com>
2021-02-27 19:01:31 +0100

    fixes codacy error

--- src/web/pages/_app.tsx
+++ src/web/pages/_app.tsx
@@ -2,5 +2,5 @@ import "../styles/slant.css";
 
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  };
+    return <Component {...pageProps} />;
+  }
