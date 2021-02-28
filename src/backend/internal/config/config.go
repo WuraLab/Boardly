57062ca878f5633ce6490ccac6ceb6b5a44ccc47
Tunde Oladipupo <simplytunde@gmail.com>
2021-02-28 14:40:18 -0600

    removed comments

--- src/backend/internal/config/config.go
+++ src/backend/internal/config/config.go
@@ -24,22 +24,6 @@ type Server struct {
 	Port string `mapstructure:"SERVER_PORT"`
 }
 
-// type Config struct {
-// 	Database struct {
-// 		Host        string `yaml:"host" env:"DB_HOST" env-description:"Database host" env-upd`
-// 		Port        string `yaml:"port" env:"DB_PORT" env-description:"Database port" env-upd`
-// 		Username    string `yaml:"username" env:"DB_USER" env-description:"Database user name" env-upd`
-// 		Password    string `env:"DB_PASSWORD" env-description:"Database user password" env-upd`
-// 		Name        string `yaml:"db-name" env:"DB_NAME" env-description:"Database name" env-upd`
-// 		Connections int    `yaml:"connections" env:"DB_CONNECTIONS" env-description:"Total number of database connections"  env-upd`
-// 	} `yaml:"database"`
-// 	Server struct {
-// 		Host string `yaml:"host" env:"SRV_HOST,HOST" env-description:"Server host" env-default:"localhost"  env-upd`
-// 		Port string `yaml:"port" env:"SRV_PORT,PORT" env-description:"Server port" env-default:"8080"`
-// 	} `yaml:"server"`
-// 	Greeting string `env:"GREETING" env-description:"Greeting phrase" env-default:"Hello!"  env-upd`
-// }
-
 func LoadConfig(configFile string, paths ...string) (Config, error) {
 	config := Config{}
 	viper.SetConfigName(configFile)
