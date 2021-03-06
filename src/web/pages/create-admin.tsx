36736a8b017d50fb361e000941e4223c09227b49
Fawas kareem <phawazzzy@gmail.com>
2021-03-06 14:53:39 +0100

    improved UI

--- src/web/pages/create-admin.tsx
+++ src/web/pages/create-admin.tsx
@@ -3,8 +3,74 @@ import Grid from "@material-ui/core/Grid";
 import { Container, FormControl, Button, Typography } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
 import "@fontsource/poppins";
+import InputLabel from '@material-ui/core/InputLabel';
+import {
+    createStyles,
+    fade,
+    Theme,
+    withStyles,
+    makeStyles,
+} from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+
+const BootstrapInput = withStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            'label + &': {
+                marginTop: theme.spacing(3),
+            },
+        },
+        input: {
+            borderRadius: 4,
+            position: 'relative',
+            backgroundColor: 'rgba(196, 196, 196, 0.2)',
+            border: '1px solid black',
+            fontSize: 16,
+            width: '55vh',
+            padding: '10px 12px',
+            transition: theme.transitions.create(['border-color', 'box-shadow']),
+            // Use the system font instead of the default Roboto font.
+            fontFamily: [
+                '-apple-system',
+                'BlinkMacSystemFont',
+                '"Segoe UI"',
+                'Roboto',
+                '"Helvetica Neue"',
+                'Arial',
+                'sans-serif',
+                '"Apple Color Emoji"',
+                '"Segoe UI Emoji"',
+                '"Segoe UI Symbol"',
+            ].join(','),
+            '&:focus': {
+                boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+                borderColor: theme.palette.primary.main,
+            },
+        },
+    }),
+)(InputBase);
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+            flexWrap: 'wrap',
+        },
+        margin: {
+            margin: theme.spacing(1),
+        },
+        input: {
+            fontWeight: "bold",
+            fontSize: "20px",
+            fontFamily: "poppins",
+            color: "black"
+        }
+    }),
+);
+
 
 const CreateAdmin = () => {
+    const classes = useStyles();
     return (
         <Box padding={0} margin={0}>
             <Grid container spacing={0}>
@@ -15,29 +81,35 @@ const CreateAdmin = () => {
                     <Container style={{ height: "100vh", backgroundColor: "white", display: "flex", alignItems: "center", margin: "50px 0 0 0" }} >
 
                         <Grid item container direction="column" alignItems="center">
-                            <Typography style={{ color: "#000000", fontWeight:"bold", fontSize: "30px", letterSpacing: "4px"}}>
+                            <Typography style={{ color: "#000000", fontWeight: "bold", fontSize: "30px", letterSpacing: "4px" }}>
                                 Create Admin Account
                             </Typography>
-                            <Typography style={{ color: "#867979"}}>Fill out  the folowing information  to create an admin account</Typography>
+                            <Typography style={{ color: "#867979" }}>Fill out  the folowing information  to create an admin account</Typography>
                             <br />
-                            <FormControl>
-                                <TextField variant="outlined" label="Company's name" style={{ width: "60vh", }} />
+                            <FormControl className={classes.margin}>
+                                <InputLabel shrink htmlFor="bootstrap-input" className= {classes.input} >
+                                    Company's Name
+                                </InputLabel>
+                                <BootstrapInput id="bootstrap-input"  placeholder="Wuralab" />
                             </FormControl>
-                            <br />
-                            <FormControl>
-                                <TextField variant="outlined" label="Company's Email" style={{ width: "60vh", }} />
+                            <FormControl className={classes.margin}>
+                                <InputLabel shrink htmlFor="bootstrap-input" className= {classes.input} >
+                                    Company's Email
+                                </InputLabel>
+                                <BootstrapInput id="bootstrap-input" placeholder="user@wuralab.com" />
                             </FormControl>
-                            <br />
-                            <FormControl>
-                                <TextField variant="outlined" label="Password" style={{ width: "60vh", }} />
+                            <FormControl className={classes.margin}>
+                                <InputLabel shrink htmlFor="bootstrap-input" className= {classes.input} >
+                                    Password
+                                </InputLabel>
+                                <BootstrapInput id="bootstrap-input"  placeholder="password"/>
                             </FormControl>
-                            <br /> <br />
-
+                            < br />
                             <Button variant="contained" style={{ backgroundColor: "#22ad80", color: "#ffffff", width: "50vh" }} >
                                 Create Account
                             </Button>
                             <Typography color="primary">
-                               <a href="/login"> Have an account?  <span> Login </span>  </a>
+                                <a href="/login"> Have an account?  <span> Login </span>  </a>
                             </Typography>
                         </Grid>
 
