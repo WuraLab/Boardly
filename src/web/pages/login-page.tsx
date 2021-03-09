8ee07c19161640ffb6dd0e8ed843db1934bb5ab6
oluwaseun <dadsclement@gmail.com>
2021-03-09 21:49:10 +0100

    login UI in progress

--- src/web/pages/login-page.tsx
+++ src/web/pages/login-page.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import { Input, InputLabel, FormLabel, TextField, Link } from "@material-ui/core";
+import { Container, FormControl, Button, Typography } from "@material-ui/core";
+import "@fontsource/poppins";
+import InputLabel from "@material-ui/core/InputLabel";
+import {
+    createStyles,
+    fade,
+    Theme,
+    withStyles,
+    makeStyles,
+} from "@material-ui/core/styles";
 import InputBase from "@material-ui/core/InputBase";
 
 const BootstrapInput = withStyles((theme: Theme) =>
@@ -56,85 +61,66 @@ const useStyles = makeStyles((theme: Theme) =>
 
 
 const Login = () => {
+    const classes = useStyles();
+    return (
+        <Box padding={0} margin={0}>
+            <Grid container spacing={0}>
+                <Grid item xs={6}>
+                    <Box style= {{ margin: "20px 0 20px 100px"}}>
+                        <img src="/boardly_logo.png" width="100" height="100" />
+                    </Box>
+                    <Container style={{ height: "100vh", backgroundColor: "white", display: "flex", margin: "50px 0 0 0" }} >
 
-  const classes = useStyles();
+                        <Grid item container direction="column" alignItems="center">
+                            <Typography style={{ color: "#000000", fontWeight: "bold", fontSize: "30px", letterSpacing: "4px" }}>
+                            Sign in to Boardly
+                            </Typography>
+                           
+                            <br />
+                            
+                            
+                            <FormControl className={classes.margin}>
+                                <InputLabel shrink htmlFor="bootstrap-input" className={classes.input} >
+                                    Company's Email
+                                </InputLabel>
+                                <BootstrapInput id="bootstrap-input" placeholder="user@wuralab.com" />
+                            </FormControl>
+                            <FormControl className={classes.margin}>
+                                <InputLabel shrink htmlFor="bootstrap-input" className={classes.input} >
+                                    Password
+                                </InputLabel>
+                                <BootstrapInput id="bootstrap-input" placeholder="password" />
+                            </FormControl>
+                           
+                            <Typography color="primary">
+                                <a href="/create-admin"> Forget Password? </a>
+                            </Typography>  <br/>  <br/>
+                            <Button variant="contained" style={{ backgroundColor: "#22ad80", color: "#ffffff", width: "50vh" }} >
+                                Sign in
+                            </Button>
+                            <Typography color="primary">
+                            New here? <a href="/create-admin"> <span> Create an account </span>  </a>
+                            </Typography>
+                        </Grid>
 
-  return (
-    
-          <Box padding={0} margin={0}>
-              <Grid container style={{ backgroundColor: "#EEEAEA" }} spacing={0}>
-                  <Grid item xs={6}>
-            
-                  <Container style={{ height: "100vh", backgroundColor: "#EEEAEA",   margin: "150px 0px 0px 50px",}}>
-                      <Typography component="h1" variant="h4" style={{fontFamily: "poppins", fontSize: 40, fontWeight: "bold"}}>
-            Sign in to Boardly
-          </Typography>
-
-                            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              style={{ width: "60vh", }} 
-              id="email"
-              label="Company's Email"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              style={{ width: "60vh", }} 
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-
-                        
-<Typography>
-  <a href="#"> Forgot password?</a>
-   
-    </Typography>
-    <br /> <br />
-  
-
-                          <Button variant="contained" style ={{backgroundColor: "#22ad80", color: "#ffffff", width: "60vh"}}
-              type="submit">
-              Sign In
-            </Button> 
-
-
-
-            
-<Typography> New here? 
- <a href="/create-admin">  Create an account </a>
-  </Typography>
-
-</Container>
-                     
-                  </Grid>
-                  <Grid item xs={6}>
-                      <Box>
-                      <Container style={{ height: "100vh", backgroundColor: "#753FF6",  clipPath: "polygon(8% 0, 100% 0, 100% 100%, 0% 100%)"  }}>
-                          <div style={{
+                    </Container>
+                </Grid>
+                <Grid item xs={6}>
+                    <Box>
+                        <Container style={{ height: "100vh", backgroundColor: "#753ff6", clipPath: "polygon(8% 0, 100% 0, 100% 100%, 0% 100%)" }}>
+                            <div style={{
                                 margin: "0 50px 0 50px",
-                                position: 'absolute', left: '50%', top: '50%',
-                                transform: 'translate(10%, -50%)'
-                            }} >
-                              <img src="/window.png"/>
-  
-                          </div>
-                      </Container>
-                      </Box>
-                  </Grid>
-              </Grid>
-  
-          </Box>
-  );
+                                position: "absolute", left: "50%", top: "50%",
+                                transform: "translate(10%, -50%)"
+                            }}>
+                                <img src="/window.png" />
+                            </div>
+                        </Container>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box >
+    );
 };
+
 export default Login;
-
-
