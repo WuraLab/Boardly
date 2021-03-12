214a20f5a45b46c9c99e14cea3f0c4ba26514336
Tunde Oladipupo <simplytunde@gmail.com>
2021-03-11 22:32:36 -0600

    removed test not needed

--- src/backend/internal/models/user_test.go
+++ src/backend/internal/models/user_test.go
@@ -14,22 +14,4 @@ func TestUser(t *testing.T) {
 		assert.NoError(t, err, "Error create user in DB")
 		assert.Equal(t, rows, 1, "Rows return is not 1")
 	})
-	t.Run("UpdateName", func(t *testing.T) {
-		u.FirstName = "Peter"
-		assert.NoError(t, u.UpdateName(DB), "Error updating name in DB")
-	})
-	t.Run("UpdatePassword", func(t *testing.T) {
-		u.Password = "newpassword"
-		assert.NoError(t, u.UpdatePassword(DB), "Error updating password in DB")
-	})
-	t.Run("UpdateEmail", func(t *testing.T) {
-		u.Email = "newemail@gmail.com"
-		assert.NoError(t, u.UpdateEmail(DB), "Error updating email in DB")
-	})
-	t.Run("Save", func(t *testing.T) {
-		assert.NoError(t, u.Save(DB), "Error saving User in DB")
-	})
-	t.Run("Delete", func(t *testing.T) {
-		assert.NoError(t, u.Delete(DB), "Error deleting user from DB")
-	})
 }
