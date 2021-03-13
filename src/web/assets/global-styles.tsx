0ebc0448368600e000d495cba088b1dded0033bc
nextwebb <oaikhenahpeterson@gmail.com>
2021-03-13 19:59:39 +0100

    fixed Unexpected character ('}' (code 125)): was expecting double-quote to start field name

--- src/web/assets/global-styles.tsx
+++ src/web/assets/global-styles.tsx
@@ -3,36 +3,36 @@ import { createStyles, makeStyles } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles(() =>
-  createStyles({
-    '@global': {
-      html: {
-        '-webkit-font-smoothing': 'antialiased',
-        '-moz-osx-font-smoothing': 'grayscale',
-        height: '100%',
-        width: '100%'
-      },
-      '*, *::before, *::after': {
-        boxSizing: 'inherit'
-      },
-      body: {
-        height: '100%',
-        width: '100%',
-        margin :"0px",
-        background: "E5E5E5",
-        fontFamily: "Poppins"
-      },
-    //   '#root': {
-    //     height: '100%',
-    //     width: '100%'
-    //   }
-    }
-  })
+    createStyles({
+        '@global': {
+            html: {
+                '-webkit-font-smoothing': 'antialiased',
+                '-moz-osx-font-smoothing': 'grayscale',
+                height: '100%',
+                width: '100%'
+            },
+            '*, *::before, *::after': {
+                boxSizing: 'inherit'
+            },
+            body: {
+                height: '100%',
+                width: '100%',
+                margin: '0px',
+                background: 'E5E5E5',
+                fontFamily: 'Poppins'
+            }
+            //   '#root': {
+            //     height: '100%',
+            //     width: '100%'
+            //   }
+        }
+    })
 );
 
 const GlobalStyles: React.FC = () => {
-  useStyles();
+    useStyles();
 
-  return null;
+    return null;
 };
 
 export default GlobalStyles;
