9f50d3604577c22051f4bb29bfe0035b9297549d
robogeek95 <azeezlukman95@gmail.com>
2021-03-18 11:06:39 +0100

    created app theme

---
+++ src/web/pages/_app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import theme from '../theme';
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    return (
+        <MuiThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </MuiThemeProvider>
+    );
+}
+
+export default MyApp;
---
+++ src/web/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
+import pink from '@material-ui/core/colors/pink';
+
+export default createMuiTheme({
+  palette: {
+    primary: pink,
+    secondary: indigo
+  }
+});
