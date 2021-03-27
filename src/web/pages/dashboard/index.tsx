f8ff07a3089bf26b61b5be5dee62cd8578069a86
peterson oaikhenah <oaikhenahpeterson@gmail.com>
2021-03-27 01:58:19 +0100

    components modules

---
+++ src/web/pages/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardView: React.FC = () => {
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    );
+};
+
+export default DashboardView;
