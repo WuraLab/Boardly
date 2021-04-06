9c804cbaa4d6ec8c0c1ab165a4830705740e5e71
peterson oaikhenah <oaikhenahpeterson@gmail.com>
2021-04-06 07:34:48 +0100

    dashboard template

--- src/web/components/common/SideBar/Sidebar.tsx
+++ src/web/components/common/SideBar/Sidebar.tsx
@@ -1,69 +1,153 @@
 import React from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import { Grid, Box, Typography } from '@material-ui/core';
-import Logo from '../../../public/logo.svg';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const drawerWidth = 240;
+
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            flexGrow: 1,
-            position: 'fixed',
-            height: '100%',
-            width: '100%',
-            margin: '0px',
-            padding: '0px',
-            top: '0px',
-            backgroundColor: '#16093C'
+            display: 'flex'
         },
-
-        navItem: {
-            // backgroundColor: '#FFFFFF',
-            color: '#ffff',
-            border: '1px solid red',
-            width: '100%'
+        drawer: {
+            [theme.breakpoints.up('sm')]: {
+                width: drawerWidth,
+                flexShrink: 0
+            }
+        },
+        appBar: {
+            [theme.breakpoints.up('sm')]: {
+                width: `calc(100% - ${drawerWidth}px)`,
+                marginLeft: drawerWidth
+            }
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+            [theme.breakpoints.up('sm')]: {
+                display: 'none'
+            }
+        },
+        // necessary for content to be below app bar
+        toolbar: theme.mixins.toolbar,
+        drawerPaper: {
+            width: drawerWidth
+        },
+        content: {
+            flexGrow: 1,
+            padding: theme.spacing(3)
         }
     })
 );
 
-export const Sidebar: React.FC = () => {
+interface Props {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window?: () => Window;
+}
+
+export const Sidebar: React.FC = (props: Props) => {
+    const { window } = props;
     const classes = useStyles();
+    const theme = useTheme();
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const drawer = (
+        <div>
+            <div className={classes.toolbar} />
+            <Divider />
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
     return (
         <>
-            <div className={classes.root}>
-                <Grid container>
-                    <Grid item xs={12} sm={12}>
-                        <Box>
-                            <img
-                                alt="forgot password"
-                                style={{
-                                    height: '100px',
-                                    maxWidth: '100px',
-                                    marginLeft: '100px'
-                                }}
-                                src={Logo}
-                            />
-                        </Box>
-                    </Grid>
-
-                    {/* <Grid item xs={12} sm={12} className={classes.navItem}>
-                        <h1>Overview</h1>
-                    </Grid>
-
-                    <Grid item xs={12} sm={12} className={classes.navItem}>
-                        <h1>Employees</h1>
-                    </Grid> */}
-
-                    <Typography component="div">
-                        <Box fontSize="fontSize" className={classes.navItem} m={1}>
-                            dfdgdgdfgdfgd Overview
-                        </Box>
-                        <Box fontSize="fontSize" className={classes.navItem} m={1}>
-                            dfdgdgdfgdfgd Overview
-                        </Box>
+            <CssBaseline />
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        className={classes.menuButton}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        Responsive drawer
                     </Typography>
-                </Grid>
-            </div>
+                </Toolbar>
+            </AppBar>
+            <nav className={classes.drawer} aria-label="mailbox folders">
+                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                <Hidden smUp implementation="css">
+                    <Drawer
+                        container={container}
+                        variant="temporary"
+                        anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                        open={mobileOpen}
+                        onClose={handleDrawerToggle}
+                        classes={{
+                            paper: classes.drawerPaper
+                        }}
+                        ModalProps={{
+                            keepMounted: true // Better open performance on mobile.
+                        }}>
+                        {drawer}
+                    </Drawer>
+                </Hidden>
+                <Hidden xsDown implementation="css">
+                    <Drawer
+                        classes={{
+                            paper: classes.drawerPaper
+                        }}
+                        variant="permanent"
+                        open>
+                        {drawer}
+                    </Drawer>
+                </Hidden>
+            </nav>
         </>
     );
 };
--- src/web/pages/dashboard/index.tsx
+++ src/web/pages/dashboard/index.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 import { Sidebar } from '../../components/common/SideBar/Sidebar';
+import { Main } from '../../components/common/Main/Main';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            flexGrow: 1,
-            position: 'fixed',
-            height: '100%',
-            width: '100%',
-            margin: '0px',
-            padding: '0px',
-            top: '0px',
-            backgroundColor: '#FFFFFF'
+            // flexGrow: 1,
+            // position: 'fixed',
+            // height: '100%',
+            // width: '100%',
+            // margin: '0px',
+            // padding: '0px',
+            // top: '0px',
+            // backgroundColor: '#FFFFFF'
+            display: 'flex'
         },
         rightGrid: {
             position: 'relative',
@@ -25,9 +27,6 @@ const useStyles = makeStyles((theme: Theme) =>
         leftGrid: {
             height: '100%',
             backgroundColor: '#16093C',
-            // border: '1px solid red',
-            // justifyContent: 'center',
-            // alignContent: 'center',
             padding: 0,
             margin: 0
         }
@@ -39,14 +38,9 @@ const DashboardView: React.FC = () => {
         <>
             <div className={classes.root}>
                 <Grid container spacing={0}>
-                    <Grid item xs={12} sm={3} className={classes.leftGrid}>
-                        <Sidebar />
-                    </Grid>
-
-                    <Grid item xs={12} sm={9} className={classes.rightGrid}>
-                        <h1>main</h1>
-                    </Grid>
+                    <Sidebar />
                 </Grid>
+                <Main />
             </div>
         </>
     );
