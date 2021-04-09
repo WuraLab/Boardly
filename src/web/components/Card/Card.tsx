d8f94b9f9a678c307512904b27730ceabf27620e
peterson oaikhenah <oaikhenahpeterson@gmail.com>
2021-04-09 18:49:30 +0100

    implemented the created employee modal

--- src/web/components/Card/Card.tsx
+++ src/web/components/Card/Card.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -11,28 +12,58 @@ const useStyles = makeStyles((theme: Theme) =>
         paper: {
             padding: theme.spacing(2),
             textAlign: 'center',
-            color: theme.palette.text.secondary
+            color: theme.palette.text.secondary,
+            '&:hover': {
+                border: '1px solid #5A26A7',
+                color: '#5A26A7'
+            }
+        },
+        cardItem: {
+            color: '#000',
+            '&:hover': {
+                color: '#5A26A7'
+            }
         }
     })
 );
 
-export default function AutoGrid() {
+export default function Card() {
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
             <Grid container spacing={3}>
                 <Grid item xs>
-                    <Paper className={classes.paper}>xs</Paper>
+                    <Paper className={classes.paper}>
+                        <Typography variant="h6">Employees</Typography>
+                        <Typography variant="h4" className={classes.cardItem}>
+                            60
+                        </Typography>
+                    </Paper>
                 </Grid>
                 <Grid item xs>
-                    <Paper className={classes.paper}>xs</Paper>
+                    <Paper className={classes.paper}>
+                        <Typography variant="h6">Integrations</Typography>
+                        <Typography variant="h4" className={classes.cardItem}>
+                            16
+                        </Typography>
+                    </Paper>
                 </Grid>
                 <Grid item xs>
-                    <Paper className={classes.paper}>xs</Paper>
+                    <Paper className={classes.paper}>
+                        <Typography variant="h6">Accepted Invites</Typography>
+                        <Typography variant="h4" className={classes.cardItem}>
+                            43
+                        </Typography>
+                    </Paper>
                 </Grid>
                 <Grid item xs>
-                    <Paper className={classes.paper}>xs</Paper>
+                    <Paper className={classes.paper}>
+                        <Typography variant="h6">Pending Invites</Typography>
+                        <Typography variant="h4" className={classes.cardItem}>
+                            64
+                        </Typography>
+                    </Paper>
                 </Grid>
             </Grid>
         </div>
