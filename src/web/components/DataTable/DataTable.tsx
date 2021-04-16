71846eceae7605983216c942e9acb279e94e9daa
peterson oaikhenah <oaikhenahpeterson@gmail.com>
2021-04-16 21:22:10 +0100

    template for remote data and search prop

--- src/web/components/DataTable/DataTable.tsx
+++ src/web/components/DataTable/DataTable.tsx
@@ -1,53 +1,131 @@
 import React, { FC, useState } from 'react';
 import MaterialTable from 'material-table';
-import DeleteIcon from '@material-ui/icons/Delete';
-import SearchIcon from '@material-ui/icons/Search';
-import SaveIcon from '@material-ui/icons/Save';
-import { Button } from '@material-ui/core';
 
-import { useStyles } from './material-table-wrapper.styles';
+import { forwardRef } from 'react';
 
-interface Column {
-    name: string;
-    occupation: string;
-    age: number;
-}
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import image from '../../public/logo.svg';
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
 export const DataTable: FC = () => {
     // const classes = useStyles();
+    const [dataStore, setDataStore] = useState([]);
 
     return (
         <div style={{ maxWidth: '100%' }}>
             <MaterialTable
-                title="Remote Data Preview"
+                icons={tableIcons}
+                title="Basic Search Preview"
                 columns={[
                     {
                         title: 'Avatar',
                         field: 'avatar',
+                        // eslint-disable-next-line react/display-name
                         render: (rowData) => (
-                            <img style={{ height: 36, borderRadius: '50%' }} src={rowData.avatar} />
+                            <img
+                                alt="avatar"
+                                style={{ height: 36, borderRadius: '50%' }}
+                                src={rowData.avatar}
+                            />
                         )
                     },
-                    { title: 'Id', field: 'id' },
-                    { title: 'First Name', field: 'first_name' },
-                    { title: 'Last Name', field: 'last_name' }
+                    {
+                        title: 'Name',
+                        field: 'name'
+                    },
+                    {
+                        title: 'Surname',
+                        field: 'surname'
+                    },
+                    {
+                        title: 'Birth Year',
+                        field: 'birthYear'
+                    },
+                    {
+                        title: 'Birth City',
+                        field: 'birthCity'
+                    }
                 ]}
-                data={(query) =>
-                    new Promise((resolve, reject) => {
-                        let url = 'https://reqres.in/api/users?';
-                        url += 'per_page=' + query.pageSize;
-                        url += '&page=' + (query.page + 1);
-                        fetch(url)
-                            .then((response) => response.json())
-                            .then((result) => {
-                                resolve({
-                                    data: result.data,
-                                    page: result.page - 1,
-                                    totalCount: result.total
-                                });
-                            });
-                    })
-                }
+                data={[
+                    {
+                        avatar: { image },
+                        name: 'Mehmet',
+                        surname: 'Baran',
+                        birthYear: 1987,
+                        birthCity: 63
+                    },
+                    {
+                        avatar: { image },
+                        name: 'Zerya Betül',
+                        surname: 'Baran',
+                        birthYear: 2017,
+                        birthCity: 34
+                    },
+                    {
+                        avatar: { image },
+                        name: 'Mehmet',
+                        surname: 'Baran',
+                        birthYear: 1987,
+                        birthCity: 63
+                    },
+                    {
+                        avatar: { image },
+                        name: 'Zerya Betül',
+                        surname: 'Baran',
+                        birthYear: 2017,
+                        birthCity: 34
+                    },
+                    {
+                        avatar: { image },
+                        name: 'Mehmet',
+                        surname: 'Baran',
+                        birthYear: 1987,
+                        birthCity: 63
+                    },
+                    {
+                        avatar: { image },
+                        name: 'Zerya Betül',
+                        surname: 'Baran',
+                        birthYear: 2017,
+                        birthCity: 34
+                    }
+                ]}
+                options={{
+                    search: true
+                }}
             />
         </div>
     );
