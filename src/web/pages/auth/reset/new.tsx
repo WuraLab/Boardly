22933ddd660325ed14fa129cf9cddb7b60ad499d
peterson oaikhenah <oaikhenahpeterson@gmail.com>
2021-05-18 23:31:17 +0100

    correct import for reset component

--- src/web/pages/auth/reset/new.tsx
+++ src/web/pages/auth/reset/new.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD:src/web/pages/auth/reset-password.tsx
-import { ResetPassword } from '../../components/auth/resetpassword/ResetPassword';
 
-=======
-import { ResetPassword } from '../../../components/resetpassword/ResetPassword';
->>>>>>> master:src/web/pages/auth/reset/new.tsx
+import { ResetPassword } from '../../../components/auth/resetpassword/ResetPassword';
 // import { Layout } from '../../components/layout/index';
 // export const ForgotPasswordView: React.FC = () => {
 // export default function ForgotPasswordView() {
